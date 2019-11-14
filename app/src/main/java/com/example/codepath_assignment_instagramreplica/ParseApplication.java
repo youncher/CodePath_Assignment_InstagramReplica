0fef0a1fb622d5bd7c7c4eac16cf45313b1f0e3b
Cherie Young <44423254+youncher@users.noreply.github.com>
2019-11-14 10:54:40 -0800

    Added ParseApplication which tells app which backend server to talk to.

---
+++ app/src/main/java/com/example/codepath_assignment_instagramreplica/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.example.codepath_assignment_instagramreplica;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("cherie-instagram") // should correspond to APP_ID env variable
+                .clientKey(BuildConfig.CLIENT_KEY)  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("http://cherie-instagram.herokuapp.com/parse").build());
+    }
+}
